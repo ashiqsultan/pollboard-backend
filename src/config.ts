d4f9326a1e0da11d0e750c0772e24b12d0805835
ashiqsultan <ashiqdrive@gmail.com>
2022-08-19 10:22:51 +0530

    Made redis connection string as env variable

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,12 @@
 interface IConfig {
   port: string;
   corsOptions: any;
+  redisConnectionString: string;
 }
 const config: IConfig = {
   port: process.env.PORT || '7000',
   corsOptions: { origin: '*' },
+  redisConnectionString: process.env.REDIS_CONNECTION_STRING || '',
 };
 
 export default config;
--- src/redis/index.ts
+++ src/redis/index.ts
@@ -1,7 +1,8 @@
 import { createClient } from 'redis';
+import config from '../config';
 
 const redis = createClient({
-  url: 'redis://dev01:sd3232QWQW$$34KJHG@redis-11983.c274.us-east-1-3.ec2.cloud.redislabs.com:11983',
+  url: config.redisConnectionString,
 });
 
 redis.on('error', (err) => console.log('Redis Client Error', err));
